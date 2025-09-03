9a0230270d3f7b029628d006becae35bf308febf
Sergii <noodlex3m@gmail.com>
2025-09-03 20:59:38 +0300

    fix form

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -36,24 +36,27 @@ function LoginForm() {
 	}
 	return (
 		<form onSubmit={handleSubmit}>
-			<div>
-				<label htmlFor="email">Email:</label>
-				<input
-					type="email"
-					id="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-			</div>
-			<div>
-				<label htmlFor="password">Пароль:</label>
-				<input
-					type="password"
-					id="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-			</div>
+			<fieldset>
+				<legend>User info</legend>
+				<div>
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						id="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">Пароль:</label>
+					<input
+						type="password"
+						id="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+			</fieldset>
 			<button type="submit">Увійти</button>
 		</form>
 	);
